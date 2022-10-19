7f329957bedb36d389ad96830e8e0e606eedb320
Abang Ayoma <abangayoma@gmail.com>
2022-10-19 06:13:17 -0700

    Added another repo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
 /**
- * main - c code runs from here
- *Initial Description: Print _putchr
- * Return: Always (0)
- * Program to print _putchar, followed by a new line.
- * Prgram should return 0
+ * main - print a _putchar program
+ * Return: Always 0
  */
 int main(void)
 {
-	char c[8] = "_putchar";
-	int i;
-	for (i = 0; i < 8; i++)
-	{
-		_putchar (c[i]);
-	}
-	_putchar ('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
