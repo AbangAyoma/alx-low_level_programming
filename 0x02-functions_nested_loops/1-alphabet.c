19434bcf14e10e67d8a8b4b6acc4fde6eaa86b7e
Abang Ayoma <abangayoma@gmail.com>
2022-10-19 06:47:00 -0700

    added something

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,19 @@
 
 /**
  * main - check the code
+ *
  * print_alphabet - Print lowercase alphabets
  *
  * function print_alphabet :print alphabets
+ *
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
  * Return: Always (0)
  */
 void print_alphabet(void)
 {
 	char c;
-	
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
