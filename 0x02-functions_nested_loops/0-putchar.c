a4c1265ac78e1fe2127acf648669c262a409faed
Abang Ayoma <abangayoma@gmail.com>
2022-10-20 08:43:37 -0700

    Added a readme file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
 #include "main.h"
+
 /**
- * main - print a _putchar program
- * Return: Always 0
+ *main - print a _putchar program
+ *
+ * Return: Always (0)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
+	char c[8] = "_putchar";
+	int i;
+	for (i = 0; i < 8; i++)
+	{
+		_putchar (c[i]);
+	}
+	_putchar ('\n');
 	return (0);
 }
